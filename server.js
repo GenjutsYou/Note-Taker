b50c8beb8927b6996af022c81d850b1a833f95cb
GenjutsYou <125905469+GenjutsYou@users.noreply.github.com>
2023-07-17 22:19:33 -0700

    redo

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.use(express.static('public'));
 
 // HTML Routes
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/notes.html'));
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, '/puclic/index.html'));
 });
 
 // API Routes
